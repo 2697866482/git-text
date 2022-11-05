67d2fec8f87fddbf8882c089d9ac75a82941f150
ssln <2697866482@qq.com>
2022-11-05 09:25:58 +0800

    first hot-fix commit

--- src/main/java/com/w/git/gitTest.java
+++ src/main/java/com/w/git/gitTest.java
@@ -4,5 +4,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("first idea git");
         System.out.println("second idea git");
+        System.out.println("first hot-fix");
     }
 }
