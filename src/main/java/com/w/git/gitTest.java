1be9932036244bdc0b5ce3cf1eacc2c8da8d28aa
ssln <2697866482@qq.com>
2022-11-05 09:10:06 +0800

    second idea commit

--- src/main/java/com/w/git/gitTest.java
+++ src/main/java/com/w/git/gitTest.java
@@ -3,5 +3,6 @@ package com.w.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("first idea git");
+        System.out.println("second idea git");
     }
 }
